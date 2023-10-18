81176e69d7b20e218c2dd4a3190e24ed5beba290
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-10-18 11:32:33 +0200

    added some crucial functionalities

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,24 @@
 const answer = Math.floor(Math.random() * 10+1);
 let guesses=0;
-document.getElementById("submit").onclick= function(){
+document.getElementById("submit").onclick = function(){
    let guess = document.getElementById("guessValue").value;
-   guesses+=1;
-   if(guess==answer){
-    document.getElementById("ans").value=`Wow you made it! The answer is: ${answer} and you took ${guesses}`;
+   if(guess!=0){
+    guesses+=1;
+     if(guess==answer){
+        document.getElementById("ans").innerHTML=`Hat's off! The answer is: ${answer} and you took ${guesses} guesses`;
+        document.getElementById("wrong").innerHTML="";
+     }
+    else if(guess < answer){
+       document.getElementById("wrong").innerHTML="Your answer is too small! Try again!";
+       document.getElementById("ans").innerHTML="";
+    }
+    else{
+       document.getElementById("wrong").innerHTML="Your answer is too large! Try again!";
+       document.getElementById("ans").innerHTML="";
    }
-   else if(guess < answer){
-    document.getElementById("ans").value=`Your answer is too small`;
-   }
-   else{
-    document.getElementById("ans").value=`Your answer is too large`;
-   }
+}
+else{
+    document.getElementById("wrong").innerHTML="Ooops! No guess made!";
+    document.getElementById("ans").innerHTML="";
+}
 }
